f8d81ddd0cfcd12d53bf19b7edfde4c07b21a2b8
xeulbn <gsbtiger0215@gmail.com>
2024-07-29 14:44:56 +0900

    initial commit

---
+++ src/main/java/org/example/grip_demo/GripDemoApplication.java
@@ -0,0 +1,13 @@
+package org.example.grip_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GripDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GripDemoApplication.class, args);
+    }
+
+}
