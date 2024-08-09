39dafc581082b9c307fa43b939be03e88ebc5838
jongwoolee <jong168@naver.com>
2024-08-09 10:43:42 +0900

    search 기능

--- src/main/java/org/example/grip_demo/map/interfaces/MapRestController.java
+++ src/main/java/org/example/grip_demo/map/interfaces/MapRestController.java
@@ -4,39 +4,28 @@ import lombok.extern.slf4j.Slf4j;
 import org.example.grip_demo.climbinggym.application.ClimingGymService;
 import org.example.grip_demo.climbinggym.domain.ClimbingGym;
 import org.example.grip_demo.climbinggym.interfaces.ClimbingGymDto;
-import org.example.grip_demo.condemo.ConDemoService;
-import org.example.grip_demo.condemo.CongestionDemoDto;
 import org.example.grip_demo.map.application.SearchApiService;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 @RestController
 @Slf4j
 public class MapRestController {
+    private final SearchApiService searchApiService;
     private final ClimingGymService climingGymService;
-    private final ConDemoService conDemoService;
 
-    public MapRestController(ClimingGymService climingGymService, ConDemoService conDemoService) {
+    public MapRestController(SearchApiService naverApiService, ClimingGymService climingGymService) {
+        this.searchApiService = naverApiService;
         this.climingGymService = climingGymService;
-        this.conDemoService = conDemoService;
     }
-
 
     @GetMapping("/api/climbinggyms")
     public List<ClimbingGymDto> main() {
         List<ClimbingGym> gyms = climingGymService.getAllClimbingGyms();
         return gyms.stream()
-                .map(gym -> new ClimbingGymDto(gym.getId(), gym.getName(),
-                        gym.getAddress(), gym.getMapX(), gym.getMapY(),
-                        gym.getAcceptableCount(),
-                        gym.getCongestions().stream()
-                                .map(con -> new CongestionDemoDto(con.getTimeZone(), con.getPresentCount()))
-                                .collect(Collectors.toList())
-                ))
+                .map(gym -> new ClimbingGymDto(gym.getId(), gym.getName(), gym.getAddress(), gym.getMapX(), gym.getMapY()))
                 .collect(Collectors.toList());
     }
 
@@ -44,20 +33,7 @@ public class MapRestController {
     public List<ClimbingGymDto> searchGyms(@RequestParam("keyword") String keyword) {
         List<ClimbingGym> gyms = climingGymService.searchClimbingGyms(keyword);
         return gyms.stream()
-                .map(gym -> new ClimbingGymDto(gym.getId(), gym.getName(),
-                gym.getAddress(), gym.getMapX(), gym.getMapY(),
-                gym.getAcceptableCount(),
-                gym.getCongestions().stream()
-                        .map(con -> new CongestionDemoDto(con.getTimeZone(), con.getPresentCount()))
-                        .collect(Collectors.toList())
-                ))
+                .map(gym -> new ClimbingGymDto(gym.getId(), gym.getName(), gym.getAddress(), gym.getMapX(), gym.getMapY()))
                 .collect(Collectors.toList());
     }
-//    @GetMapping("/climbinggym/{gymId}/congestion")
-//    public ResponseEntity<CongestionDemoDto> getCurrentCongestion(@PathVariable Long gymId, @RequestParam int hour) {
-//        Optional<CongestionDemoDto> congestion = conDemoService.get(gymId, hour);
-//        return congestion.map(ResponseEntity::ok)
-//                .orElseGet(() -> ResponseEntity.notFound().build());
-//    }
-
 }
---
+++ src/main/java/org/example/grip_demo/search/application/SearchService.java
@@ -0,0 +1,28 @@
+package org.example.grip_demo.search.application;
+
+import lombok.RequiredArgsConstructor;
+import org.example.grip_demo.climbinggym.domain.ClimbingGym;
+import org.example.grip_demo.post.domain.Post;
+import org.example.grip_demo.search.infrastructure.SearchClimbingGymRepository;
+import org.example.grip_demo.search.infrastructure.SearchPostRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class SearchService {
+    private final SearchPostRepository searchPostRepository;
+    private final SearchClimbingGymRepository searchClimbingGymRepository;
+
+    public Page<Post> searchPostContaining(String keyword, int page, int size) {
+        Pageable pageable = PageRequest.of(page, size);
+        return searchPostRepository.findByTitleContaining(keyword, pageable);
+    }
+    public Page<ClimbingGym> searchClimbingGymContaining(String keyword, int page, int size) {
+        Pageable pageable = PageRequest.of(page, size);
+        return searchClimbingGymRepository.findByNameContaining(keyword, pageable);
+    }
+
+}
