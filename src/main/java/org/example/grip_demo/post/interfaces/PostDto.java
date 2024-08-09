c20502ad2ad501392e1dd3b78aaec8c5ae0120c3
xeulbn <gsbtiger0215@gmail.com>
2024-08-09 17:59:13 +0900

    comment add

--- src/main/java/org/example/grip_demo/post/interfaces/PostDto.java
+++ src/main/java/org/example/grip_demo/post/interfaces/PostDto.java
@@ -3,7 +3,10 @@ package org.example.grip_demo.post.interfaces;
 import lombok.Getter;
 import lombok.Setter;
 import org.example.grip_demo.climbinggym.domain.ClimbingGym;
+import org.example.grip_demo.comment.interfaces.CommentDTO;
 import org.example.grip_demo.user.domain.User;
+
+import java.util.List;
 
 @Getter
 @Setter
@@ -14,6 +17,7 @@ public class PostDto {
     private Integer viewCount;
     private Integer likeCount;
     private ClimbingGym climbingGym;
-
     private User user;
+
+    private List<CommentDTO> comments;
 }
