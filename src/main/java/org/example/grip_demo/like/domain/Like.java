cd1b099d155ac52e3347cff6d7b5d4631af18b1a
Kim-ByeongGyu <zenk5554785@gmail.com>
2024-08-19 15:09:48 +0900

    make Like entity

--- src/main/java/org/example/grip_demo/like/domain/Like.java
+++ src/main/java/org/example/grip_demo/like/domain/Like.java
@@ -1,4 +1,24 @@
 package org.example.grip_demo.like.domain;
 
+import jakarta.persistence.*;
+import org.example.grip_demo.post.domain.Post;
+import org.example.grip_demo.user.domain.User;
+
+@Entity(name = "USER_POST_LIKES")
 public class Like {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "USER_ID", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "POST_ID", nullable = false)
+    private Post post;
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
