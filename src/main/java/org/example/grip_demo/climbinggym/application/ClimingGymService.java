03dc6f517424dc4bb7e21566606b67e1c7828574
Kim-ByeongGyu <zenk5554785@gmail.com>
2024-08-26 15:08:55 +0900

    클라이밍 짐 페이지 css 적용 및 페이징 처리

--- src/main/java/org/example/grip_demo/climbinggym/application/ClimingGymService.java
+++ src/main/java/org/example/grip_demo/climbinggym/application/ClimingGymService.java
@@ -2,7 +2,12 @@ package org.example.grip_demo.climbinggym.application;
 
 import org.example.grip_demo.climbinggym.domain.ClimbingGym;
 import org.example.grip_demo.climbinggym.domain.ClimbingGymRepository;
+import org.example.grip_demo.climbinggym.domain.PageablePostRepository;
+import org.example.grip_demo.post.domain.Post;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -12,10 +17,12 @@ import java.util.stream.Collectors;
 @Service
 public class ClimingGymService {
     private final ClimbingGymRepository climbingGymRepository;
+    private final PageablePostRepository pageablePostRepository;
 
     @Autowired
-    public ClimingGymService(ClimbingGymRepository climbingGymRepository) {
+    public ClimingGymService(ClimbingGymRepository climbingGymRepository, PageablePostRepository pageablePostRepository) {
         this.climbingGymRepository = climbingGymRepository;
+        this.pageablePostRepository = pageablePostRepository;
     }
 
     public ClimbingGym createClimbingGym(ClimbingGym climbingGym) {
@@ -42,5 +49,12 @@ public class ClimingGymService {
 
     public void deleteClimbingGym(Long id) {
         climbingGymRepository.deleteById(id);
+    }
+
+    public Page<Post> pageClimbingGymPosts(Long id, int page, int size) {
+        Pageable pageable = PageRequest.of(page, size);
+        Page<Post> byPostPage = pageablePostRepository.findByClimbingGym_Id(id, pageable);
+
+        return byPostPage;
     }
 }
